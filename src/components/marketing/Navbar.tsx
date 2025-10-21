26491dadc2fd4f27db7cf2d7b73c6ecade25256e
Honey <arjunkamboj87699@gmail.com>
2025-10-21 14:06:24 +0530

    final form

---
+++ src/components/marketing/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Logo } from "@/components/Logo";
+import { Button } from "@heroui/button";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div className=" border-b border-default-200  py-3.5">
+      <div className="flex items-center container mx-auto justify-between">
+        <Logo />
+
+        <Button as={Link} href="/overview" color="primary" variant="solid">
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
+};
